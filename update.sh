410ce7cebe3f949b4d8f8c031e68594035bb51c6
Dave Pirotte <dpirotte@gmail.com>
2017-08-07 22:46:46 -0400

    only update when there is a synctoken present

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 while sleep 120; do
   wget -q https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
 
-  if [[ $(git status -s ip-ranges.json) ]]; then
+  if [[ $(git status -s ip-ranges.json && grep syncToken ip-ranges.json) ]]; then
     git add ip-ranges.json
     git commit -m 'updating ip-ranges.json'
     git push

1207d402a4067ac9a8daf13c35f2273401dcf194
Dave Pirotte <dpirotte@gmail.com>
2018-01-25 23:19:09 -0500

    sqs testing

--- sqs/test.sh
+++ sqs/test.sh
@@ -8,35 +8,30 @@ QURL="https://queue.amazonaws.com/788222722721/$QUEUE"
 
 $SQS send-message --queue-url $QURL --message-body "$(cat sqs/test.json)" > /dev/null
 
-MSG=$($SQS receive-message --queue-url $QURL --wait-time-seconds 20)
 
-if [ ! -z "$MSG" ]; then
-  ID=$(echo $MSG | jq -r ".Messages[0].ReceiptHandle")
-  BODY=$(echo $MSG | jq -r ".Messages[0].Body")
-  MD5=$(echo $BODY | jq -r ".md5")
-  TOKEN=$(echo $BODY | jq -r ".syncToken")
-  URL=$(echo $BODY | jq -r .url)
+while sleep 1; do
+  MSG=$($SQS receive-message --queue-url $QURL --wait-time-seconds 20)
 
-  wget -q https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
-  JSON_MD5=$(md5sum ip-ranges.json | awk '{print $1}')
+  if [ ! -z "$MSG" ]; then
+    echo "Received $MSG" | ts
+    ID=$(echo $MSG | jq -r ".Messages[0].ReceiptHandle")
+    BODY=$(echo $MSG | jq -r ".Messages[0].Body")
+    MD5=$(echo $BODY | jq -r ".md5")
+    TOKEN=$(echo $BODY | jq -r ".syncToken")
+    URL=$(echo $BODY | jq -r .url)
 
-  if [[ "$MD5" == "$JSON_MD5" && $(git status -s ip-ranges.json) ]]; then
-    git add ip-ranges.json
-    git commit -m "updating ip-ranges.json"
-    git push
+    wget -q https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
+    JSON_MD5=$(md5sum ip-ranges.json | awk '{print $1}')
 
-    $SQS delete-message --queue-url $QURL --receipt-handle $ID
-  else
-    echo "downloaded but md5 does not match or file is not updated"
+    if [[ "$MD5" == "$JSON_MD5" && $(git status -s ip-ranges.json) ]]; then
+      git add ip-ranges.json
+      git commit -m "updating ip-ranges.json"
+      git push
+
+      $SQS delete-message --queue-url $QURL --receipt-handle $ID
+    else
+      echo "downloaded but md5 does not match or file is not updated" | ts
+    fi
   fi
-fi
-
-
-# JSON=$(curl -s $URL)
-# JSON_MD5=$(echo $JSON | md5sum)
-# echo $JSON
-# echo $JSON_MD5
-# echo $MD5
-
-# sleep 0.5
+end
 

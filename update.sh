d074b69086ad9ce7ea3020330e688c541ee3e6ab
Dave Pirotte <dpirotte@gmail.com>
2018-01-25 23:06:52 -0500

    use if-none-match header

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 while sleep 120; do
-  wget -q https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
+  wget -q \
+    --header "If-None-Match: \"$(test -e ip-ranges.json && md5sum ip-ranges.json | awk '{print $1}')\"" \
+    https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
 
-  if [[ $(git status -s ip-ranges.json && grep syncToken ip-ranges.json) ]]; then
+  if [[ $? == 0 && $(git status -s ip-ranges.json && grep syncToken ip-ranges.json) ]]; then
     git add ip-ranges.json
     git commit -m 'updating ip-ranges.json'
     git push

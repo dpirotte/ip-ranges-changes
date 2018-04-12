fe23832de28f3d750238f5587c03d55ca6969374
Dave Pirotte <dpirotte@gmail.com>
2018-04-11 23:39:46 -0400

    fix bash script with if-none-match

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while sleep 120; do
-  wget -q --header "If-None-Match: \"$(test -e ip-ranges.json && md5sum ip-ranges.json | awk '{print $1}')\"" \
-    https://ip-ranges.amazonaws.com/ip-ranges.json
+  wget -q -O - --header "If-None-Match: \"$(test -e ip-ranges.json && md5sum ip-ranges.json | awk '{print $1}')\"" \
+    https://ip-ranges.amazonaws.com/ip-ranges.json | ifne sh -c "cat - > ip-ranges.json"
 
   if [[ $? == 0 && $(git status -s ip-ranges.json && grep syncToken ip-ranges.json) ]]; then
     git add ip-ranges.json

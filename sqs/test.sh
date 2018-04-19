b662f305b7087cdfd306bfc3df044db4929a72cc
Dave Pirotte <dpirotte@gmail.com>
2018-04-18 22:12:40 -0400

    tmp

--- sqs/test.sh
+++ sqs/test.sh
@@ -20,13 +20,13 @@ while sleep 1; do
     TOKEN=$(echo $BODY | jq -r ".syncToken")
     URL=$(echo $BODY | jq -r .url)
 
-    # wget -q https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
+    wget -q https://ip-ranges.amazonaws.com/ip-ranges.json -O ip-ranges.json
     JSON_MD5=$(md5sum ip-ranges.json | awk '{print $1}')
 
     # if [[ "$MD5" == "$JSON_MD5" && $(git status -s ip-ranges.json) ]]; then
-    #   git add ip-ranges.json
-    #   git commit -m "updating ip-ranges.json"
-    #   git push
+      git add ip-ranges.json
+      git commit -m "updating ip-ranges.json"
+      git push
 
       $SQS delete-message --queue-url $QURL --receipt-handle $ID
     # else
